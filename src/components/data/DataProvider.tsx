f124b2aff0d9eac6d296e6f842ba2c60728300b7
Jacob Lurye <jlurye@jimmy.harvard.edu>
2019-10-24 16:40:54 -0400

    Actually fix the no files found bug :/

--- src/components/data/DataProvider.tsx
+++ src/components/data/DataProvider.tsx
@@ -36,8 +36,7 @@ export const DataProvider: React.FunctionComponent = props => {
 
     React.useEffect(refreshData, []);
 
-    const value =
-        files === undefined ? undefined : { files, dataStatus, refreshData };
+    const value = files && { files, dataStatus, refreshData };
 
     return (
         <DataContext.Provider value={value}>
