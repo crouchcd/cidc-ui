bf9b835cffc54b6c26b70e161edd11dc9482be86
Jacob Lurye <jlurye@jimmy.harvard.edu>
2020-04-29 16:11:52 -0400

    Eve-SQLAlchemy removal updates (#219)
    * Update api client to integrate eve-sqlalchemy-removal changes
    
    * Clean up param-building code
    
    * Update tests
    
    * Revert .env changes

--- src/components/browseFiles/__tests__/FileTable.ts
+++ src/components/browseFiles/__tests__/FileTable.ts
@@ -1,41 +1,50 @@
 import {
-    filtersToWhereClause,
-    headerToSortClause,
-    triggerBatchDownload
+    triggerBatchDownload,
+    filterParamsFromFilters,
+    sortParamsFromHeader
 } from "../FileTable";
 import { getDownloadURL } from "../../../api/api";
 jest.mock("../../../api/api");
 
-test("filtersToWhereClause", () => {
-    expect(filtersToWhereClause({})).toBe("(analysis_friendly==true)");
-    expect(filtersToWhereClause({ raw_files: true })).toBe("");
-    expect(filtersToWhereClause({ upload_type: [1, 2], raw_files: true })).toBe(
-        '(upload_type=="1" or upload_type=="2")'
-    );
+test("filterParamsFromFilters", () => {
+    expect(filterParamsFromFilters({})).toEqual({
+        analysis_friendly: true,
+        trial_ids: undefined,
+        upload_types: undefined
+    });
+    expect(filterParamsFromFilters({ raw_files: true })).toEqual({
+        analysis_friendly: false,
+        trial_ids: undefined,
+        upload_types: undefined
+    });
     expect(
-        filtersToWhereClause({
-            upload_type: [1, 2],
-            trial_id: undefined,
-            raw_files: true
-        })
-    ).toBe('(upload_type=="1" or upload_type=="2")');
+        filterParamsFromFilters({ upload_type: [1, 2], raw_files: true })
+    ).toEqual({
+        analysis_friendly: false,
+        trial_ids: undefined,
+        upload_types: "1,2"
+    });
     expect(
-        filtersToWhereClause({
+        filterParamsFromFilters({
             upload_type: [1, 2],
             trial_id: ["a", "b"]
         })
-    ).toBe(
-        '(trial_id=="a" or trial_id=="b") and (upload_type=="1" or upload_type=="2") and (analysis_friendly==true)'
-    );
+    ).toEqual({
+        analysis_friendly: true,
+        trial_ids: "a,b",
+        upload_types: "1,2"
+    });
 });
 
-test("headerToSortClause", () => {
-    expect(headerToSortClause({ key: "foo", direction: "asc" })).toBe(
-        '[("foo", 1)]'
-    );
-    expect(headerToSortClause({ key: "foo", direction: "desc" })).toBe(
-        '[("foo", -1)]'
-    );
+test("sortParamsFromHeader", () => {
+    expect(sortParamsFromHeader({ key: "foo", direction: "asc" })).toEqual({
+        sort_field: "foo",
+        sort_direction: "asc"
+    });
+    expect(sortParamsFromHeader({ key: "foo", direction: "desc" })).toEqual({
+        sort_field: "foo",
+        sort_direction: "desc"
+    });
 });
 
 test("triggerBatchDownload", async done => {
