3981afa2e4fb36a57591325ebfe6a0bad571d977
Jacob Lurye <jlurye@jimmy.harvard.edu>
2020-09-25 10:27:22 -0400

    Simplify AuthProvider component and tests (#272)
    * Simplify AuthProvider component and tests
    
    * /callback should never redirect to /callback
    
    * Add missing dependency from onRedirectCallback

--- src/components/identity/AuthProvider.test.tsx
+++ src/components/identity/AuthProvider.test.tsx
@@ -1,202 +1,128 @@
 import * as React from "react";
-import { render } from "@testing-library/react";
+import { render, cleanup, act } from "@testing-library/react";
 import AuthProvider, {
     auth0Client,
-    setSession,
-    handleAuthentication
+    IAuthData,
+    AuthContext
 } from "./AuthProvider";
-import auth0 from "auth0-js";
+import auth0, { Auth0Callback } from "auth0-js";
 import { Router } from "react-router";
 import history from "./History";
-import { Location } from "history";
 jest.mock("auth0-js");
 
 auth0.WebAuth.mockImplementation(() => ({
     authorize: jest.fn(),
     logout: jest.fn(),
-    checkSession: jest.fn(),
-    parseHash: jest.fn()
+    checkSession: jest.fn()
 }));
+
+const authResponse = {
+    idToken: "test-token",
+    idTokenPayload: {
+        email: "test@email.com",
+        given_name: "john",
+        family_name: "doe"
+    }
+};
+
+const mockSignedIn = () => {
+    auth0Client.checkSession.mockImplementation(
+        (_: any, cb: Auth0Callback<any>) => {
+            cb(null, authResponse);
+        }
+    );
+};
 
 const ChildComponent = () => <div data-testid="children" />;
 
-function renderWithChild() {
+function renderWithChild(child: React.ReactElement = <ChildComponent />) {
     return render(
         <Router history={history}>
-            <AuthProvider>
-                <ChildComponent />
-            </AuthProvider>
+            <AuthProvider>{child}</AuthProvider>
         </Router>
     );
 }
 
-beforeEach(() => {
-    localStorage.clear();
-});
-
 afterEach(() => {
-    jest.clearAllMocks();
+    jest.resetAllMocks();
 });
 
-it("triggers login and doesn't render the app", () => {
+it("triggers login when login is required", () => {
+    auth0Client.checkSession.mockImplementation(
+        (_: any, cb: Auth0Callback<any>) => {
+            cb({ code: "login_required" }, {});
+        }
+    );
     const { queryByTestId } = renderWithChild();
-    expect(auth0Client.authorize).toBeCalledTimes(1);
+    expect(auth0Client.checkSession).toHaveBeenCalled();
+    expect(auth0Client.authorize).toHaveBeenCalled();
     expect(queryByTestId("children")).not.toBeInTheDocument();
 });
 
-it("handles requests to '/callback' as expected", async () => {
-    const { findByTestId } = renderWithChild();
-    history.replace("/callback");
-    const callbackLoader = await findByTestId("callback-loader");
-    expect(callbackLoader).toBeInTheDocument();
-    expect(auth0Client.parseHash).toBeCalledTimes(1);
+it("silently authenticates and provides userInfo and token to children", async done => {
+    mockSignedIn();
+
+    const TestChild = (props: { expects: (authData?: IAuthData) => void }) => {
+        const authData = React.useContext(AuthContext);
+        React.useEffect(() => {
+            if (authData) {
+                props.expects(authData);
+                done();
+            }
+        }, [authData]);
+        return null;
+    };
+
+    renderWithChild(
+        <TestChild
+            expects={authData => {
+                expect(authData?.idToken).toBe(authResponse.idToken);
+                expect(authData?.user.email).toBe(
+                    authResponse.idTokenPayload.email
+                );
+                expect(authData?.user.first_n).toBe(
+                    authResponse.idTokenPayload.given_name
+                );
+                expect(authData?.user.last_n).toBe(
+                    authResponse.idTokenPayload.family_name
+                );
+            }}
+        />
+    );
 });
 
-it("handles requests to '/logout' as expected", async () => {
-    history.replace("/logout");
+it("redirects from the '/callback' route on successful login", async () => {
+    mockSignedIn();
 
-    // Set up a fake session
-    localStorage.setItem("isLoggedIn", "true");
-    localStorage.setItem("expiresAt", "99999999999");
+    // callback with no target URL
+    history.replace("/callback");
+    await renderWithChild().findByTestId("children");
+    expect(history.location.pathname).toBe("/");
+    cleanup();
 
-    const { queryByTestId } = renderWithChild();
+    // callback with target URL
+    const returnTo = "/some/path?with=query";
+    history.replace(`/callback?returnTo=${returnTo}`);
+    await renderWithChild().findByTestId("children");
+    expect(history.location.pathname + history.location.search).toBe(returnTo);
+    cleanup();
 
-    const children = queryByTestId("children");
-    expect(children).not.toBeInTheDocument();
+    // callback with bad target URL "/callback"
+    history.replace("/callback?returnTo=/callback");
+    await renderWithChild().findByTestId("children");
+    expect(history.location.pathname).toBe("/");
+});
 
-    // Check that session was cleared
-    expect(localStorage.getItem("isLoggedIn")).toBeNull();
-    expect(localStorage.getItem("expiresAt")).toBeNull();
+it("calls logout when pathname is '/logout'", async () => {
+    mockSignedIn();
+
+    const { findByTestId } = renderWithChild();
+    await findByTestId("children");
+    expect(auth0Client.logout).toBeCalledTimes(0);
+
+    act(() => {
+        history.replace("/logout");
+    });
 
     expect(auth0Client.logout).toBeCalledTimes(1);
-    expect(auth0Client.checkSession).not.toBeCalled();
-});
-
-it("handles silent auth as expected", () => {
-    history.replace("/");
-
-    // Set up a fake session with expired token
-    localStorage.setItem("isLoggedIn", "true");
-    localStorage.setItem("expiresAt", "0");
-
-    renderWithChild();
-    // User should be logged in silently, not explicitly
-    expect(auth0Client.checkSession).toBeCalledTimes(1);
-    expect(auth0Client.authorize).toBeCalledTimes(0);
-
-    auth0Client.checkSession.mockClear();
-
-    // Set up logged-out session
-    localStorage.setItem("isLoggedIn", "false");
-
-    renderWithChild();
-    // User should be logged in explicitly, not silently
-    expect(auth0Client.checkSession).not.toBeCalled();
-    expect(auth0Client.authorize).toBeCalledTimes(1);
-});
-
-test("session setter", () => {
-    // Mock out a test sessionSetter
-    const setAuthData = jest.fn();
-    const onComplete = jest.fn();
-    const setError = jest.fn();
-    const sessionSetter = setSession(setAuthData, onComplete, setError);
-
-    const targetRoute = "/test-route";
-
-    // Well-behaved input
-    const decodedHash = {
-        idTokenPayload: {
-            email: "a",
-            given_name: "b",
-            family_name: "c",
-            exp: 1
-        },
-        idToken: "foobar"
-    };
-    sessionSetter(decodedHash, targetRoute);
-
-    const expectedUser = {
-        email: decodedHash.idTokenPayload.email,
-        first_n: decodedHash.idTokenPayload.given_name,
-        last_n: decodedHash.idTokenPayload.family_name
-    };
-    expect(setAuthData).toHaveBeenCalledWith({
-        idToken: decodedHash.idToken,
-        user: expectedUser
-    });
-    expect(history.location.pathname).toEqual(targetRoute);
-    expect(setError).not.toHaveBeenCalled();
-
-    // Reset
-    history.replace("/");
-    jest.clearAllMocks();
-
-    // Input with missing scopes
-    const decodedHashWithMissingScopes = {
-        idTokenPayload: {
-            email: "a",
-            exp: 1
-        },
-        idToken: "foobar"
-    };
-    sessionSetter(decodedHashWithMissingScopes, targetRoute);
-    expect(setError).toHaveBeenCalledWith({
-        type: "Login Error",
-        message: "userinfo missing required scope(s)"
-    });
-    expect(onComplete).not.toHaveBeenCalled();
-
-    // Reset
-    history.replace("/");
-    jest.clearAllMocks();
-
-    // Hash with missing fields
-    sessionSetter({}, targetRoute);
-    expect(setError).toHaveBeenCalledWith({
-        type: "Login Error",
-        message: "cannot set session: missing id token"
-    });
-    expect(onComplete).not.toHaveBeenCalled();
-});
-
-test("handleAuthentication", () => {
-    const sessionSetter = jest.fn();
-    const setError = jest.fn();
-    const targetRoute = "/test-route";
-
-    // Good inputs: complete auth result, and location with "next" param
-    const authResult = {
-        accessToken: "a",
-        idToken: "b"
-    };
-    auth0Client.parseHash = jest.fn((handler: any) =>
-        handler(undefined, authResult)
-    );
-    const location = { search: `?next=${targetRoute}` } as Location;
-    handleAuthentication(location, sessionSetter, setError);
-    expect(auth0Client.parseHash).toHaveBeenCalled();
-    expect(sessionSetter).toHaveBeenCalledWith(authResult, targetRoute);
-    expect(setError).not.toHaveBeenCalled();
-
-    jest.clearAllMocks();
-
-    // Missing "next" param on location
-    const locationMissingNext = { search: "" } as Location;
-    handleAuthentication(locationMissingNext, sessionSetter, setError);
-    expect(auth0Client.parseHash).toHaveBeenCalled();
-    expect(sessionSetter).toHaveBeenCalledWith(authResult, "/");
-    expect(setError).not.toHaveBeenCalled();
-
-    jest.clearAllMocks();
-
-    // Missing fields in authResult
-    auth0Client.parseHash = jest.fn((handler: any) => handler(undefined, {}));
-    handleAuthentication(location, sessionSetter, setError);
-    expect(auth0Client.parseHash).toHaveBeenCalled();
-    expect(sessionSetter).not.toHaveBeenCalled();
-    expect(setError).toHaveBeenCalledWith({
-        type: "Login Error",
-        message: "authentication failed"
-    });
 });
