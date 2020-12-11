63f67ee3789753d204c2cc807e2031cf2a20ed7c
Jacob Lurye <jlurye@jimmy.harvard.edu>
2020-12-11 12:56:36 -0500

    Add admin interface for editing user's contact emails (#335)
    * Add field for editing contact emails to admin table
    
    * Move interactive fields next to each other
    
    * Add tests
    
    * Fix tests

--- src/components/profile/AdminUserManager.test.tsx
+++ src/components/profile/AdminUserManager.test.tsx
@@ -20,6 +20,7 @@ const user = users[0];
 const currentUser = users[users.length - 1];
 
 beforeEach(() => {
+    jest.clearAllMocks();
     getUsers.mockResolvedValue({ data: users, meta: { total: users.length } });
     getUserEtag.mockResolvedValue("test-etag");
     updateUser.mockImplementation(async (t, i, e, updates) => {
@@ -43,6 +44,10 @@ it("renders all users", async () => {
     });
 });
 
+const getUserUpdates = () => {
+    return updateUser.mock.calls[0][3];
+};
+
 test("role update selection", async () => {
     const newRole = "cidc-admin";
 
@@ -55,26 +60,69 @@ test("role update selection", async () => {
     // Change the user's role to cidc-admin
     fireEvent.mouseDown(roleSelect);
     fireEvent.click(getByText(newRole));
-    waitFor(() =>
-        expect(updateUser).toHaveReturnedWith({ ...user, role: newRole })
-    );
+    await waitFor(() => expect(updateUser).toHaveBeenCalled());
+    expect(getUserUpdates()).toEqual({ role: newRole });
 });
 
 test("disabling a user", async () => {
-    const { findAllByTitle, getByText } = renderWithUserContext(
+    const { findAllByRole } = renderWithUserContext(
         <AdminUserManager />,
         currentUser
     );
 
-    const disableToggle = (await findAllByTitle(/disable this account/i))[0];
+    const disableToggle = (await findAllByRole("checkbox"))[0];
 
     // Disable the user
     fireEvent.click(disableToggle);
-    waitFor(() =>
-        expect(updateUser).toHaveReturnedWith({ ...user, disabled: true })
+    await waitFor(() => expect(updateUser).toHaveBeenCalled());
+    expect(getUserUpdates()).toEqual({ disabled: true });
+});
+
+test("adding a contact email", async () => {
+    const { findAllByText, getByPlaceholderText } = renderWithUserContext(
+        <AdminUserManager />,
+        currentUser
     );
 
-    // Re-enable the user
-    fireEvent.click(disableToggle);
-    waitFor(() => expect(updateUser).toHaveLastReturnedWith(user));
+    // Add a new contact email
+    const contactEmail = "some@contact.email.com";
+    const addButton = (await findAllByText(/add a contact email/i))[0];
+    fireEvent.click(addButton);
+    const emailInput = getByPlaceholderText(/add a contact email/i);
+    fireEvent.input(emailInput, { target: { value: contactEmail } });
+    fireEvent.submit(emailInput);
+
+    // Check that updates were made
+    await waitFor(() => expect(updateUser).toHaveBeenCalled());
+    expect(getUserUpdates()).toEqual({ contact_email: contactEmail });
+});
+
+test("editing a contact email", async () => {
+    const oldContactEmail = "old@contact.email.com";
+    const userWithContactEmail = {
+        ...users[0],
+        contact_email: oldContactEmail
+    };
+    getUsers.mockResolvedValue({
+        data: [userWithContactEmail, ...users.slice(1)],
+        meta: { total: users.length }
+    });
+
+    const {
+        findByText,
+        findByTitle,
+        getByPlaceholderText
+    } = renderWithUserContext(<AdminUserManager />, currentUser);
+
+    expect(await findByText(oldContactEmail)).toBeInTheDocument();
+
+    // Updating an existing contact email
+    const newContactEmail = "new@contact.email.com";
+    const editButton = await findByTitle(/edit contact email/i);
+    fireEvent.click(editButton);
+    const emailInput = getByPlaceholderText(/add a contact email/i);
+    fireEvent.input(emailInput, { target: { value: newContactEmail } });
+    fireEvent.submit(emailInput);
+    await waitFor(() => expect(updateUser).toHaveBeenCalled());
+    expect(getUserUpdates()).toEqual({ contact_email: newContactEmail });
 });
--- src/components/profile/AdminUserManager.tsx
+++ src/components/profile/AdminUserManager.tsx
@@ -11,16 +11,111 @@ import {
     MenuItem,
     Select,
     Switch,
-    Button
+    Button,
+    TextField,
+    Box,
+    IconButton,
+    Grid
 } from "@material-ui/core";
 import { getUserEtag, getUsers, updateUser } from "../../api/api";
 import { Account } from "../../model/account";
-import { SupervisorAccount } from "@material-ui/icons";
+import { Edit, SupervisorAccount } from "@material-ui/icons";
 import PaginatedTable, { ISortConfig } from "../generic/PaginatedTable";
 import { useUserContext } from "../identity/UserProvider";
 import { withIdToken } from "../identity/AuthProvider";
 import { ORGANIZATION_NAME_MAP, ROLES } from "../../util/constants";
 import UserPermissionsDialogWithInfo from "./AdminUserPermissionsDialog";
+import { useForm } from "react-hook-form";
+
+const useContactEmailStyles = makeStyles(theme => ({
+    input: {
+        fontSize: theme.typography.subtitle2.fontSize,
+        width: 200
+    }
+}));
+
+const ContactEmail: React.FC<{
+    user: Account;
+    onSave: (newContactEmail: string) => void;
+}> = ({ user, onSave }) => {
+    const classes = useContactEmailStyles();
+    const [isEditing, setIsEditing] = React.useState<boolean>(false);
+    const { register, handleSubmit } = useForm<{ contactEmail: string }>();
+    return (
+        <Box>
+            {isEditing ? (
+                <form
+                    onSubmit={handleSubmit(({ contactEmail }) => {
+                        if (contactEmail) {
+                            onSave(contactEmail);
+                        }
+                        setIsEditing(false);
+                    })}
+                >
+                    <TextField
+                        autoFocus
+                        inputRef={register}
+                        inputProps={{
+                            name: "contactEmail",
+                            defaultValue: user.contact_email
+                        }}
+                        InputProps={{ className: classes.input }}
+                        placeholder="Add a contact email"
+                        variant="outlined"
+                        size="small"
+                    />
+                    <Grid container wrap="nowrap" alignItems="center">
+                        <Grid item>
+                            <Button size="small" color="primary" type="submit">
+                                update
+                            </Button>
+                        </Grid>
+                        <Grid item>
+                            <Button
+                                size="small"
+                                onClick={() => setIsEditing(false)}
+                            >
+                                cancel
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </form>
+            ) : (
+                <div>
+                    {user.contact_email ? (
+                        <Grid
+                            container
+                            spacing={1}
+                            wrap="nowrap"
+                            alignItems="center"
+                        >
+                            <Grid item>{user.contact_email}</Grid>
+                            <Grid item>
+                                <Tooltip title="edit contact email">
+                                    <IconButton
+                                        size="small"
+                                        onClick={() => setIsEditing(true)}
+                                    >
+                                        <Edit fontSize="small" />
+                                    </IconButton>
+                                </Tooltip>
+                            </Grid>
+                        </Grid>
+                    ) : (
+                        <Button
+                            size="small"
+                            variant="outlined"
+                            color="primary"
+                            onClick={() => setIsEditing(true)}
+                        >
+                            add a contact email
+                        </Button>
+                    )}
+                </div>
+            )}
+        </Box>
+    );
+};
 
 const useRowStyles = makeStyles(theme => ({
     disabled: {
@@ -54,6 +149,13 @@ const AdminUserTableRow: React.FC<IAdminUserTableRowProps> = withIdToken(
 
         return (
             <>
+                <TableCell className={cellClass}>{user.email}</TableCell>
+                <TableCell className={cellClass}>
+                    {user.first_n} {user.last_n}
+                </TableCell>
+                <TableCell className={cellClass}>
+                    {ORGANIZATION_NAME_MAP[user.organization]}
+                </TableCell>
                 <TableCell>
                     <Tooltip
                         title={
@@ -72,12 +174,13 @@ const AdminUserTableRow: React.FC<IAdminUserTableRowProps> = withIdToken(
                         />
                     </Tooltip>
                 </TableCell>
-                <TableCell className={cellClass}>{user.email}</TableCell>
-                <TableCell className={cellClass}>
-                    {user.first_n} {user.last_n}
-                </TableCell>
-                <TableCell className={cellClass}>
-                    {ORGANIZATION_NAME_MAP[user.organization]}
+                <TableCell>
+                    <ContactEmail
+                        user={user}
+                        onSave={contactEmail =>
+                            doUserUpdate({ contact_email: contactEmail })
+                        }
+                    />
                 </TableCell>
                 <TableCell>
                     <FormControl
@@ -171,10 +274,11 @@ const AdminUserManager: React.FC<{ token: string }> = ({ token }) => {
                             setSortConfig({ key, direction })
                     }}
                     headers={[
-                        { key: "disabled", label: "Enabled?" },
                         { key: "email", label: "Email" },
                         { key: "first_n", label: "Name" },
                         { key: "organization", label: "Organization" },
+                        { key: "disabled", label: "Enabled?" },
+                        { key: "contact_email", label: "Contact Email" },
                         { key: "role", label: "Role" },
                         { key: "", label: "Permissions" }
                     ]}
